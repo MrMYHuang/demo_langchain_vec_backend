ce0979ca409ba0f3e8fa757ba0686907bf2518be
Meng-Yuan Huang <myh@live.com>
2025-10-31 20:48:33 +0800

    * fix reload bug

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     uvicorn.run(
         "demo_langchain_vec:app",
         host="0.0.0.0", port=8000, reload=True,
-        reload_includes=["server.py", "demo_rag.py"], reload_excludes="*.py")
+        reload_includes=["server.py", "demo_langchain_vec.py"], reload_excludes="*.py")
